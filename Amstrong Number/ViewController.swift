b0352570e0b7333568decc055458462aff35ee1a
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-05-06 21:40:28 +0530

    Initial Commit

---
+++ Amstrong Number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amstrong Number
+//
+//  Created by JIJO G OOMMEN on 06/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
