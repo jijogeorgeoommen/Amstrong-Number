56c103ff57c7d1aa5cfa054be34d2752315a7967
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-09-19 11:22:56 +0530

    initial

--- Amstrong Number/ViewController.swift
+++ Amstrong Number/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var n : Int?
+    var numb : Int?
+    var reminder : Int?
+    var sum : Int=0
 
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var output: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func proceed(_ sender: Any) {
+        
+        n = Int(number.text!)
+        numb = n
+        while (n! > 0){
+        reminder = n! % 10
+        n = n! / 10
+    sum = sum + ( reminder! * reminder! * reminder!)
+         print(sum)
+            
+            }
+        if (sum == numb){
+            
+            output.text = "AMSTRONG NUMBER"
+       
+        }
+        else {
+            output.text = "NOT AMSTRONG NUMBER"
+        }
+        
+    }
+    
 }
 
